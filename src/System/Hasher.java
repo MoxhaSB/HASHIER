db8d81ea8cf8dbbac27cdaff8003e24c54407fed
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2024-09-27 16:32:18 -0300

    Methods finished

--- src/System/Hasher.java
+++ src/System/Hasher.java
@@ -4,7 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Clase que contiene los creadores de hash
+ * Hasher class
  */
 public class Hasher {
 
@@ -14,6 +14,12 @@ public class Hasher {
      */
     Hasher(){}
 
+    /**
+     * Method that create a hash with format sha-256
+     * @param contra the word to create the hash
+     * @return the hash
+     * @throws NoSuchAlgorithmException exception
+     */
     public String createHash_SHA_256(String contra) throws NoSuchAlgorithmException {
 
         MessageDigest crearHash = MessageDigest.getInstance("SHA-256");
@@ -30,6 +36,13 @@ public class Hasher {
         }
         return hexString.toString();
     }
+
+    /**
+     * Method that create a hash with format sha-512
+     * @param contra the word to create the hash
+     * @return the hash
+     * @throws NoSuchAlgorithmException exception
+     */
     public String createHash_SHA_512(String contra) throws NoSuchAlgorithmException {
         MessageDigest crearHash = MessageDigest.getInstance("SHA-512");
         byte[] bytes = crearHash.digest(contra.getBytes(StandardCharsets.UTF_8));
@@ -45,6 +58,13 @@ public class Hasher {
         }
         return hexString.toString();
     }
+
+    /**
+     * Method that create a hash with format MD5
+     * @param contra the cord to create the hash
+     * @return the hash
+     * @throws NoSuchAlgorithmException exception
+     */
     public String createHash_MD5(String contra) throws NoSuchAlgorithmException {
         MessageDigest crearHash = MessageDigest.getInstance("MD5");
         byte[] bytes = crearHash.digest(contra.getBytes(StandardCharsets.UTF_8));
@@ -60,6 +80,13 @@ public class Hasher {
         }
         return hexString.toString();
     }
+
+    /**
+     * Method that create a hash with format sha-1
+     * @param contra the word to create the hash
+     * @return the hash
+     * @throws NoSuchAlgorithmException exception
+     */
     public String createHash_SHA_1(String contra) throws NoSuchAlgorithmException {
         MessageDigest crearHash = MessageDigest.getInstance("SHA-1");
         byte[] bytes = crearHash.digest(contra.getBytes(StandardCharsets.UTF_8));
