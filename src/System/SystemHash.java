54f2e0d115f5699b7155ab44d5a96298715a0595
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2024-09-27 22:40:57 -0300

    system finished

--- src/System/SystemHash.java
+++ src/System/SystemHash.java
@@ -1,6 +1,11 @@
 package System;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SystemHash {
@@ -9,25 +14,43 @@ public class SystemHash {
     public final Scanner read = new Scanner(System.in);
     public final Helper helper = new Helper();
     public final Hasher hasher = new Hasher();
+    public final List<String> txtFiles;
+    public final Path carpeta = Paths.get("Archives");
 
-    SystemHash() throws NoSuchAlgorithmException {
+    SystemHash() throws NoSuchAlgorithmException, IOException {
+        this.txtFiles = helper.getTxtFiles();
         menu();
     }
-    public void menu() throws NoSuchAlgorithmException {
+    public void menu() throws NoSuchAlgorithmException, IOException {
 
+        System.out.println("\n\n" +
+                "░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░  \n" +
+                "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \n" +
+                "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \n" +
+                "░▒▓████████▓▒░▒▓████████▓▒░░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░▒▓██████▓▒░ ░▒▓███████▓▒░  \n" +
+                "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \n" +
+                "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \n" +
+                "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ \n" +
+                "                                                                                      \n" +
+                "                                                                                      \n");
         while(true){
-            System.out.println("\nHASHIER");
+
             System.out.println("""
+                    \n---------------------------------------------
                     1) Create a HASH
                     2) Find password with hash
-                    3) Exit
+                    3) See saved txt files
+                    4) Open directory
+                    5) Exit
                     Write your option: 
                     """);
             switch (helper.verifyData(read)){
                 case -1 -> System.out.println("\nEnter a numeric value. ");
                 case 1-> createHash();
                 case 2-> findPasswordWithHash();
-                case 3-> {return;}
+                case 3-> seeSavedTxtFile();
+                case 4-> {System.out.println("Opening... ");helper.openDirectory(carpeta);}
+                case 5-> {return;}
                 default-> System.out.println("\nWrite a valid option. ");
             }
         }
@@ -50,6 +73,13 @@ public class SystemHash {
             helper.readArchivesTxt(hash,creator);
         }
     }
+    public void seeSavedTxtFile(){
+        System.out.println("FILES: ");
+        for(String name : txtFiles){
+            System.out.println(name);
+        }
+    }
+
 
 
 
