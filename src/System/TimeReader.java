fd5e646f6da23bd48b18220adc01dd2446397bd4
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2024-09-30 13:36:06 -0300

    New class that calculates the time the program takes to find the password

---
+++ src/System/TimeReader.java
@@ -0,0 +1,33 @@
+package System;
+
+import java.time.Duration;
+import java.time.Instant;
+
+//class that read the time
+public class TimeReader {
+
+    /**
+     * Constructor
+     */
+    TimeReader(){}
+
+    /**
+     * the method create a new instant
+     * @return an instant
+     */
+    public Instant getTime(){
+        //devuelve el tiempo
+        return  Instant.now();
+    }
+
+    /**
+     * The method shows the time that passed between 2 timestamps
+     * @param start firts time
+     * @param end second time
+     */
+    public void showDuration(Instant start, Instant end){
+        // Calcular la duración en milisegundos y luego en segundos con tres decimales
+        Duration duracion = Duration.between(start, end);
+        System.out.println("|*| The word was found in " + (duracion.toMillis() / 1000.0) + " seconds |*|"); ;
+    }
+}
